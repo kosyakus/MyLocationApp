bc7bdb5615cf964269b2ed48c9fe47fec9f2eaa1
Unknown <kosyak_@mail.ru>
2017-08-28 12:16:04 +0300

    Created buttons, labels and got connections

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    
+    @IBAction func getLocation() {
+        // do nothing yet
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
