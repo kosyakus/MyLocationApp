5211ef5c82d7f362d1a6fa499dcdd79c1a413bab
Unknown <kosyak_@mail.ru>
2017-09-10 17:20:50 +0300

    Added updatelocation method which fetches the stored locations and shows on the map

--- MyLocations/MapViewController.swift
+++ MyLocations/MapViewController.swift
@@ -14,6 +14,7 @@ import CoreData
 
 class MapViewController: UIViewController {
     
+    var locations = [Location]() // var to fetch the saved in CD locations and show on the screen
     
     @IBOutlet weak var mapView: MKMapView!
     var managedObjectContext: NSManagedObjectContext!
@@ -26,6 +27,24 @@ class MapViewController: UIViewController {
     
     
     @IBAction func showLocations() {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLocations() //This fetches the Location objects and shows them on the map when the view loads
+    }
+    
+    
+//will be executed every time there is a change in the data store
+    func updateLocations() { // fetching the request
+        mapView.removeAnnotations(locations) //the locations array may already exist and may contain Location objects. If so, first remove the pins for these old objects
+        let entity = Location.entity()
+        
+        let fetchRequest = NSFetchRequest<Location>()
+        fetchRequest.entity = entity
+        
+        locations = try! managedObjectContext.fetch(fetchRequest)
+        mapView.addAnnotations(locations) //add a pin for each location on the map
     }
     
     
