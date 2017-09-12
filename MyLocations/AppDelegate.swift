4293446a64b725b576d573816de0d72a93e3ff82
Unknown <kosyak_@mail.ru>
2017-09-12 18:07:27 +0300

    Changed the tags

--- MyLocations/AppDelegate.swift
+++ MyLocations/AppDelegate.swift
@@ -33,6 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        customizeAppearance()
+        
         //first have to find the UITabBarController and then look at its viewControllers array. Once it has a reference to the CurrentLocationViewController object, it gives it the managedObjectContext
         let tabBarController = window!.rootViewController as! UITabBarController
         if let tabBarViewControllers = tabBarController.viewControllers {
@@ -112,6 +114,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         } else {
             return rootViewController
         }
+    }
+    
+    
+    //This changes the “bar tint” or background color of all navigation bars and tab bars in the app to black. also sets the color of the navigation bar’s title label to white and applies the tint color to the tab bar
+    func customizeAppearance() {
+        UINavigationBar.appearance().barTintColor = UIColor.black
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSForegroundColorAttributeName: UIColor.white ]
+        UITabBar.appearance().barTintColor = UIColor.black
+        let tintColor = UIColor(red: 255/255.0, green: 238/255.0,
+                                blue: 136/255.0, alpha: 1.0)
+        UITabBar.appearance().tintColor = tintColor
     }
 
 }
