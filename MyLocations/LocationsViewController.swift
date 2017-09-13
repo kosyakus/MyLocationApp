fb8b4e3fed5ea7ba23bfac7ecf70628961531529
Unknown <kosyak_@mail.ru>
2017-09-13 14:31:10 +0300

    Customized the look of table VC of Locations

--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -47,6 +47,13 @@ class LocationsViewController: UITableViewController {
         
         navigationItem.rightBarButtonItem = editButtonItem
         
+        //This makes the table view itself black but does not alter the cells
+        tableView.backgroundColor = UIColor.black
+        tableView.separatorColor = UIColor(white: 1.0, alpha: 0.2)
+        tableView.indicatorStyle = .white
+        
+        tableView.sectionHeaderHeight = 28
+        
         performFetch() // simolify the line below
         
    /*
@@ -89,7 +96,7 @@ class LocationsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         let sectionInfo = fetchedResultsController.sections![section]
-        return sectionInfo.name
+        return sectionInfo.name.uppercased()
     }
     
     // MARK: - UITableViewDataSource
@@ -111,6 +118,31 @@ class LocationsViewController: UITableViewController {
         cell.configure(for: location)
         
         return cell
+    }
+    
+    
+    
+    // MARK: - UITableViewDelegate
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let labelRect = CGRect(x: 15, y: tableView.sectionHeaderHeight - 14, width: 300, height: 14)
+        let label = UILabel(frame: labelRect)
+        label.font = UIFont.boldSystemFont(ofSize: 11)
+        label.text = tableView.dataSource!.tableView!(tableView, titleForHeaderInSection: section)
+        label.textColor = UIColor(white: 1.0, alpha: 0.4)
+        label.backgroundColor = UIColor.clear
+        let separatorRect = CGRect(x: 15,
+                                   y: tableView.sectionHeaderHeight - 0.5,
+                                   width: tableView.bounds.size.width - 15,
+                                   height: 0.5)
+        let separator = UIView(frame: separatorRect)
+        separator.backgroundColor = tableView.separatorColor
+        let viewRect = CGRect(x: 0, y: 0, width: tableView.bounds.size.width,
+                              height: tableView.sectionHeaderHeight)
+        let view = UIView(frame: viewRect)
+        view.backgroundColor = UIColor(white: 0, alpha: 0.85)
+        view.addSubview(label)
+        view.addSubview(separator)
+        return view
     }
     
     
